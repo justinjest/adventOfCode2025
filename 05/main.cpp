fc66fce60426ca8317c5ae962deac99febb44744
Jessica Cayaban <jest.cayaban@gmail.com>
2026-01-07 11:20:39 +0900

    Solved day 05 section 2, not an optimal solution

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <string>
@@ -6,30 +7,47 @@
 
 using namespace std;
 
-struct Range {
-  long start;
-  long end;
-};
-
-
-
 struct ProcessedData {
   vector<string> header; // This will get us the data to determine what is fresh
   vector<string> stock;  // This is what is in inventory
-  vector<Range> ranges;
+  vector<vector<long>> ranges;
 
   void unpackHeader() {
-    vector<Range> output {};
+    vector<vector<long>> output {};
     for (int i = 0; i < header.size(); i++) {
        vector<string> strRange {split(header[i], "-")};
        long start = stol(strRange[0]);
        long end = stol(strRange[1]);
-       Range out {start, end};
+       vector<long> out {start, end};
        output.push_back(out);
     }
-    ranges = output;
+
+    ranges = mergeRanges(output);
   };
 
+  // This function comes from https://www.geeksforgeeks.org/dsa/merging-intervals/
+  // I do not understand it as well as I should
+  vector<vector<long>> mergeRanges(vector<vector<long>> &arr) {
+    int n = arr.size();
+
+    sort(arr.begin(), arr.end());
+    vector<vector<long>> res;
+
+    for (int i = 0; i < n; i++) {
+      long start = arr[i][0];
+      long end = arr[i][1];
+
+      if (!res.empty() && res.back()[1] >= end) {
+        continue;
+      }
+      for (int j = i + 1; j < n; j++) {
+        if (arr[j][0] <= end)
+          end = max(end, arr[j][1]);
+      }
+      res.push_back({start, end});
+    }
+    return res;
+  }
 
   // Splits a string at a delimiter
   auto split(std::string& s, const std::string& delim)
@@ -48,17 +66,18 @@ struct ProcessedData {
 
   bool isInRange(long num) {
     for (long i = 0; i < ranges.size(); i++) {
-      if (num >= ranges[i].start && num <= ranges[i].end) {
+      if (num >= ranges[i][0] && num <= ranges[i][1]) {
         return true;
       }
     }
     return false;
   }
+
 };
 
 void printProcessedData(ProcessedData pd) {
   for (double i = 0; i < pd.ranges.size(); i++){
-    cout << pd.ranges[i].start << " - " << pd.ranges[i].end << '\n';
+    cout << pd.ranges[i][0] << " - " << pd.ranges[i][1] << '\n';
   }
   for (double i = 0; i < pd.stock.size(); i++){
     cout << pd.stock[i] << '\n';
@@ -88,12 +107,7 @@ auto preProcess (string fileName) -> ProcessedData {
   return output;
 };
 
-auto main() -> int {
-
-  ProcessedData data {};
-  data = preProcess("input05.txt");
-  cout << "Pre processing finished\n";
-  printProcessedData(data);
+int countFresh(ProcessedData data) {
   long numFresh { 0 };
   for (long i = 0; i < data.stock.size(); i++){
     if(data.isInRange(stol(data.stock[i]))) {
@@ -102,5 +116,26 @@ auto main() -> int {
     }
   }
   cout << numFresh << " fresh ingredients found\n";
+  return numFresh;
+};
+
+int countFreshIngredients(ProcessedData data) {
+  long totalFresh {0};
+  for (long i = 0; i < data.ranges.size(); i++) {
+    cout << data.ranges[i][0] << " - " << data.ranges[i][1] << "\n";
+    totalFresh += data.ranges[i][1] - data.ranges[i][0] + 1;
+  }
+  cout << totalFresh << " potentially fresh ingredients\n";
+  return totalFresh;
+}
+
+auto main() -> int {
+
+  ProcessedData data {};
+  data = preProcess("input05.txt");
+  cout << "Pre processing finished\n";
+  printProcessedData(data);
+  countFresh(data);
+  countFreshIngredients(data);
   return 0;
 };
