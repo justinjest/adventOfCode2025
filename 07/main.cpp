dc80a84a18cf4038d5f8012b2960dbe7b1cabe44
Jessica Cayaban <jest.cayaban@gmail.com>
2026-01-09 10:32:05 +0900

    Refactored to allow for recursion more simply

--- 07/main.cpp
+++ 07/main.cpp
@@ -9,6 +9,11 @@ struct Result {
   int splits;
 };
 
+struct SplitLine {
+  std::string newLine;
+  int splits;
+};
+
 auto openFile(std::string fileName) -> std::vector<std::string>{
 
   std::ifstream myfile(fileName);
@@ -48,9 +53,9 @@ std::string splitLineFirst (std::string& oldLine) {
   return newLine;
 }
 
-std::string splitLine (std::string& oldLine, std::string& currLine) {
+SplitLine splitLine (std::string& oldLine, std::string& currLine) {
   std::string newLine (currLine.length(), '.');
-  static int splits {};
+  int splits {};
   for (int i = 0; i < currLine.length(); ++i) {
     char cCurr = currLine[i];
     char cLast = oldLine[i];
@@ -81,57 +86,29 @@ std::string splitLine (std::string& oldLine, std::string& currLine) {
       break;
     }
   }
-  std::cout << "Splits this line " << splits << '\n';
-  return newLine;
+  SplitLine output {newLine, splits};
+  return output;
 }
 
 Result processData2(std::vector<std::string> input) {
-  std::vector<std::string> output { input };
+  std::vector<std::string> output {input};
   static int splits {};
-  output[0] = splitLineFirst(input[0]);
+  output[0] = splitLineFirst(output[0]);
   long unsigned len { output.size() };
   for (int j = 1; j < len; ++j) {
-    for (int i = 0; i < output[j].length(); ++i) {
-      char cCurr = output[j][i];
-      char cLast = output[j-1][i];
-      switch (cCurr) {
-      case '.':
-        if (cCurr != '|' && cLast != '|')
-          output[j][i] = '.';
-        else if (cLast == '|' || cCurr == '|') {
-          output[j][i] = '|';
-        }
-        break;
-      case 'S':
-        output[j][i] = '|';
-        break;
-      case '^':
-        if (cLast == '|'){
-          ++splits;
-          output[j][i-1] = '|';
-          output[j][i] = '^';
-          output[j][i+1] = '|';
-        } else {
-          output[j][i] = '^';
-        }
-        break;
-      case '|':
-        output[j][i] = '|';
-        break;
-      }
+    SplitLine temp {splitLine(output[j-1], output[j])};
+    output[j] = temp.newLine;
+    splits += temp.splits;
     }
-  }
-  std::cout << "Splits this line " << splits << '\n';
-  printStringVector(output);
   Result res {output, splits};
   return res;
 }
 
-std::vector<std::string> processDataPart1 (std::vector<std::string> input) {
+std::vector<std::string> processData1 (std::vector<std::string> input) {
   std::vector<std::string> output { input };
   output[0] = splitLineFirst(output[0]);
   for (int i = 1; i < output.size(); ++i) {
-    output[i] = splitLine(output[i-1], output[i]);
+    output[i] = splitLine(output[i-1], output[i]).newLine;
   }
   return output;
 }
@@ -141,6 +118,10 @@ auto main() -> int {
 
   std::vector<std::string> rawData {openFile("example.txt")};
   std::cout << "Output\n";
-  Result datapart2 = processData2(rawData);
+  //std::vector<std::string> datapart1 = processData1(rawData);
+  //printStringVector(datapart1);
+  Result datapart2 {processData2(rawData)};
+  printStringVector(datapart2.result);
+  std::cout << "Splits this line " << datapart2.splits << '\n';
   return 0;
 }
